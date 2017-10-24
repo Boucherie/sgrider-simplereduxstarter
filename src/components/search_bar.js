b65b965743f42baa5eb8c57c0704e566a3cb1f43
Emily Boucher <emi.boucher@gmail.com>
2017-10-23 23:23:34 -0400

    setState and controlled components with SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,7 +9,13 @@ class SearchBar extends Component {
   }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value})} />
+    </div>
+    );
   } //single argument means leading parentheses can be dropped
 }
 
