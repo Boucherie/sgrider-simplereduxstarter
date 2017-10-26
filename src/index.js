9ba51a6bfdf18b77c08fb490c58d8012660ecc69
Emily Boucher <emi.boucher@gmail.com>
2017-10-25 20:23:11 -0400

    add iframe embedded video with loading display, video list, list item components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAW9FE57Y_B1ha_EvIgiIg03gIHyrwgaBI';
 
-const App  = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
+// functional to class-based component
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // console.log(data);
+      this.setState({ videos });
+      // this.setState({ videos: videos })
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+
 }
 
   ReactDOM.render(<App />, document.querySelector(".container"));
